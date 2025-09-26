7412825d2533b18ac8ac5e9321345ab666e0cb1c
koop46 <ale.mar.vil46@gmail.com>
2025-09-26 10:07:52 +0200

    added sdl, dockerfiles, docker compose + endpoint for evaluating submission

---
+++ api/routers/submissions.py
@@ -0,0 +1,23 @@
+from fastapi import APIRouter, Depends, HTTPException
+from sqlalchemy.orm import Session
+from database import get_db
+from models import Quiz
+from schemas import SubmissionRequest, SubmissionResponse
+from typing import List
+from kirkpatrick import KirkpatrickModel
+
+router = APIRouter(
+    prefix="/submissions",
+    tags=["submission"]
+)
+
+@router.post("/submit_answers", response_model=SubmissionResponse)
+def submit_answers(submission: SubmissionRequest, db: Session = Depends(get_db)):
+
+    if not submission.pre_test or not submission.post_test:
+        raise HTTPException(status_code=404, detail="Pre-or post-test not found")
+
+    evaluator = KirkpatrickModel(submission.pre_test, submission.post_test)
+    summary = evaluator.summary()
+
+    return summary
