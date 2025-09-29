8da2fb194a35da8a6c4cbe923b1c31f3474f77a3
Einar <einar.bergviken@gmail.com>
2025-09-29 10:42:19 +0200

    Added results table

--- api/models.py
+++ api/models.py
@@ -1,6 +1,7 @@
 from sqlalchemy import Boolean, Column, DateTime, Integer, String, ForeignKey
 from database import Base
 from datetime import datetime, timezone
+from sqlalchemy.orm import relationship #added relationship import /Einar
 
 
 class User(Base):
@@ -14,6 +15,8 @@ class User(Base):
     is_admin = Column(Boolean)
     created_at = Column(DateTime, default=lambda: datetime.now(timezone.utc))
     updated_at = Column(DateTime, default=lambda: datetime.now(timezone.utc), onupdate=lambda: datetime.now(timezone.utc))
+
+    quiz_results = relationship("QuizResult", back_populates="user")
 
 
 class Question(Base):
@@ -42,4 +45,44 @@ class Quiz(Base):
     created_at = Column(DateTime, default=lambda: datetime.now(timezone.utc))
     updated_at = Column(DateTime, default=lambda: datetime.now(timezone.utc), onupdate=lambda: datetime.now(timezone.utc))
 
+    quiz_results = relationship("QuizResult", back_populates="quiz")
 
+"""
+Each row represents a user's attempt at a given quiz.
+Multiple rows may therefore reference the same session and quiz but be for different users.
+
+Fields for QuizResult:
+- id: Primary key for the quiz result entry.
+- user_id: Foreign key referencing the User who took the quiz.
+- session_id: An optional field to track the session in which the quiz was taken.
+- question_count: The total number of questions in the quiz.
+- score: The amount of correct answers the user achieved on the quiz.
+
+
+- time_taken: The time taken by the user to complete the quiz (in seconds).
+- timestamp: The date and time when the quiz was taken.
+- user: Relationship to the User model.
+- quiz: Relationship to the Quiz model. 
+
+
+"""
+
+
+class QuizResult(Base): 
+
+    __tablename__   = 'quiz_results'
+
+    id              = Column(Integer, primary_key=True)
+    user_id         = Column(Integer, ForeignKey('users.id'), nullable=False)
+    session_id      = Column(Integer, ForeignKey('session.id'), nullable=False)
+    question_count  = Column(Integer, nullable=False)
+    score           = Column(Integer, nullable=False) 
+
+    # Keep in mind we need to calculate score for to log answers in this way 
+    # It might be better to log answers and calculate score after?
+    
+
+    taken_at        = Column(DateTime, default=datetime.utcnow) #instead of timestamp we use taken_at
+    time_taken      = Column(Integer, nullable=False)  # Time taken in seconds 
+    user            = relationship("User", back_populates="quiz_results")
+    quiz            = relationship("Quiz", back_populates="quiz_results")
